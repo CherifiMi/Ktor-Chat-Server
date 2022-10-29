f209ac74234cfb0d7659ac00b10f50a67d8b5e52
CherifiMi <cherifi154949@gmail.com>
2022-10-29 13:10:54 +0100

    MessageDataSource Imp

--- src/main/kotlin/com/example/data/MessageDataSourceImp.kt
+++ src/main/kotlin/com/example/data/MessageDataSourceImp.kt
@@ -4,16 +4,16 @@ import com.example.data.models.Message
 import org.litote.kmongo.coroutine.CoroutineDatabase
 
 class MessageDataSourceImp(
-    private val db: CoroutineDatabase
-): MessageDataSource {
+    db: CoroutineDatabase
+) : MessageDataSource {
 
     private val messages = db.getCollection<Message>("chat1")
 
     override suspend fun getAllMessages(): List<Message> {
-        return listOf()
+        return messages.find().descendingSort(Message::timestamp).toList()
     }
 
     override suspend fun insertMessage(message: Message) {
-
+        messages.insertOne(message)
     }
 }
