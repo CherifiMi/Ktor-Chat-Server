3e3444bd516cae5fe5f83b57158f25d7041fd8f8
CherifiMi <cherifi154949@gmail.com>
2022-10-29 11:14:20 +0100

    connect to mongoDB database

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,6 +4,25 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+@Serializable
+data class Hi(
+    @BsonId
+    val id: String= ObjectId().toString(),
+    val string: String
+)
+
+val client = KMongo
+    .createClient(
+        "mongodb+srv://mito:cherifi2003@mitocluster.bpzkl.mongodb.net/?retryWrites=true&w=majority"
+    ).coroutine
+val database = client.getDatabase("chat").getCollection<Hi>("chat1")
+
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,20 @@
 package com.example.plugins
 
+import com.example.Hi
+import com.example.database
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
+import org.litote.kmongo.json
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respond(database.find().toList())
+            database.insertOne(Hi(string = "hillo")).wasAcknowledged()
         }
     }
 }
