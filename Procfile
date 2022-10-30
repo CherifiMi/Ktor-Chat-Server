271895c74e2195b38b9d95a90c45a1d4a8293cc2
CherifiMi <cherifi154949@gmail.com>
2022-10-30 02:28:02 +0100

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/com.example.ktor-chat-server/bin/com.example.ktor-chat-server
--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -19,5 +19,4 @@ fun Application.configureSecurity() {
             call.sessions.set(ChatSession(username, generateNonce()))
         }
     }
-
 }
